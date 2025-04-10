28dfe62838bd006cd916ddf781b4d4f2fe729b7b
MiuHataya <fyp.storygeneration@gmail.com>
2025-04-11 06:52:36 +0800

    Update connection.js

--- connection.js
+++ connection.js
@@ -43,7 +43,7 @@ app.post('/user', (req, res) => {
 
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'User inputは必要です！' });
+  res.status(400).json({ message: 'User inputは必要です！' });
 });
 
 
